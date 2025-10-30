745f4c02af9de69e79cc9077c3e2db2c5ff158c2
Zaid Al-Ayoubi <zaidalayoubi432@gmail.com>
2025-10-30 20:30:11 +0300

    رفت الدقة

--- YOLOv8.py
+++ YOLOv8.py
@@ -7,14 +7,26 @@ img = "people walking street.jpg"          # حط أي صورة فيها أشخ�
 results = model.predict(
     source=img,
     classes=[0],
-    conf=0.5,
-    imgsz=640,
-    device=0,   # GPU
-    half=True   # FP16
+    conf=0.4,# confidence score كل ما قلننا قاعد بطلعلي اشخاص اكثر
+    imgsz=1280, # resolution of the image بعد ما رفعتها احست الدقة كثير 
+    device=0,   # GPU5
+   # half=True   # FP16
 )
+5
+r = results[0]
+# عدل الأسماء داخل قاموس names
+r.names[0] = "person"   # أو أي اسم بدك ياه
+
+persons_count = len(r.boxes) if r.boxes is not None else 0
+print("عدد الأشخاص:", persons_count)
+
+# نرسم الصورة مع كتابة العدد عليها
+vis = r.plot()
+cv2.putText(vis, f"Persons: {persons_count}", (10, 40),
+            cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
 
 # عرض الصورة الناتجة وتثبيتها
-cv2.imshow("Result", results[0].plot())  # plot() ترسم المربعات على الصورة
+cv2.imshow("Result", vis)
 cv2.waitKey(0)  # 0 معناها انتظر ضغط أي مفتاح
 cv2.destroyAllWindows()
 # عرض
